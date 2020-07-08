12106b3460c4caafd023b6b2bebf3bc5bb1c2f8d
AnatolyRyavkin <toryavkin@gmail.com>
2020-07-08 21:52:37 +0300

    Initial Commit

---
+++ APIYAndexOAuth/APIYAndexOAuth.h
@@ -0,0 +1,19 @@
+//
+//  APIYAndexOAuth.h
+//  APIYAndexOAuth
+//
+//  Created by Anatoly Ryavkin on 08.07.2020.
+//  Copyright © 2020 AnatolyRyavkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIYAndexOAuth.
+FOUNDATION_EXPORT double APIYAndexOAuthVersionNumber;
+
+//! Project version string for APIYAndexOAuth.
+FOUNDATION_EXPORT const unsigned char APIYAndexOAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIYAndexOAuth/PublicHeader.h>
+
+
